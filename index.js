cd6b4cd1699496ab08e0b80c5ab233822c6b3710
amaar <amaar.ahmednur@student.moringaschool.com>
2023-09-19 20:37:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 62
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4; // 4
+const numbers = [10, 15, 20, 5];
+const max = Math.max(...numbers); // 20
 
 // Write your code here
